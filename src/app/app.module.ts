9b9917d6e94b52ab3ffd60cc3ba5e4ca45544a82
StanislavBudin <zerrozauros96@list.ru>
2022-09-27 00:53:46 +0300

    add routes for header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { DestinationsComponent } from './pages/destinations/destinations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ExperienceComponent,
+    DestinationsComponent
   ],
   imports: [
     BrowserModule,
